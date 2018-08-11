d97185acc48ae1542c71a355b435c2e682b13bea
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-11 05:30:53 +0200

    SpaceCounterAdded

---
+++ LD42/Assets/_Scripts/Manager/SpaceCounter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpaceCounter : SingletonBase<SpaceCounter>
+{
+    public Animator SpaceAnim;
+
+    public int SpaceCount
+    {
+        get
+        {
+            return _spaceCount;
+        }
+        set
+        {
+            _spaceCount = value;
+            SpaceCounterText.text = _spaceCount.ToString();
+        }
+    }
+
+    private int _spaceCount;
+
+    public Text SpaceCounterText;
+
+    public override void Awake()
+    {
+        base.Awake();
+        SpaceCount = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpaceCount++;
+            SpaceAnim.SetTrigger("Animate");
+        }
+    }
+}
