fb04487f06dfc225aa2bbaa3d35fb57fbc831e0a
Unknown <bocsgab@gmail.com>
2018-08-11 15:24:41 +0200

    LabelManager for show/hide labels
    Command for turning cheatmode on / off

---
+++ LD42/Assets/_Scripts/Manager/LabelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Assets._Scripts.Manager
+{
+    public class LabelManager : SingletonBase<LabelManager>
+    {
+        public List<Label> Labels;
+
+        public void RegisterLabel(Label label)
+        {
+            Labels.Add(label);
+        }
+
+        public void DisplayLabels()
+        {
+            Labels.ForEach(x => x.ShowLabel());
+        }
+
+        public void HideLabels()
+        {
+            Labels.ForEach(x => x.HideLabel());
+        }
+    }
+}
