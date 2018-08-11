f54732d13d4c2ee5d9b65533fe04045fce5d85ed
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-11 03:21:02 +0200

    PlayerMove and checkpoint

---
+++ LD42/Assets/CheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private List<ManagedGameObject> _checkPointObject;
+
+    private void Awake()
+    {
+        _checkPointObject = GetComponentInChildren<ManagedGameObject>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().SetCheckPoint(this);
+        }
+    }
+
+    public void ResetCheckPoint()
+    {
+    }
+}
---
+++ LD42/Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour
+{
+    public float PlayerSpeed;
+    public Vector3 MoveDirection;
+    public float Gravity;
+
+
+    public PlayerState _currentPlayerState;     
+    private Rigidbody _rigidbody;
+    private CharacterController _charController;
+    private Vector3 _moveDirection;
+
+    private CheckPoint _lastCheckPoint;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _charController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        _moveDirection = MoveDirection;
+        _moveDirection.y -= Gravity * Time.deltaTime;
+
+        switch (_currentPlayerState)
+        {
+            case PlayerState.Idle:
+                break;
+            case PlayerState.Walking:
+                WalkForward();
+                break;
+            case PlayerState.Fight:
+                break;
+            case PlayerState.Talking:             
+                break;
+            default:
+                break;
+        }        
+    }
+
+    private void WalkForward()
+    {
+        _charController.Move(_moveDirection * Time.deltaTime);
+    }
+
+    public void SetCheckPoint(CheckPoint checkPoint)
+    {
+        _lastCheckPoint = checkPoint;
+    }
+
+    [ContextMenu("Die")]
+    public void Die()
+    {
+        ResetToCheckPoint();
+    }
+
+    private void ResetToCheckPoint()
+    {
+        transform.position = _lastCheckPoint.transform.position;
+        _lastCheckPoint.ResetCheckPoint();
+    }
+}
+
+public enum PlayerState   
+{
+    Idle,
+    Walking,
+    Fight,
+    Talking,
+}
