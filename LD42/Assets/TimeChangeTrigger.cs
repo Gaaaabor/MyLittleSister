0a5fc412f7d6cd3a02533a8c709119f8eb0df146
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-12 22:29:55 +0200

    Full Map1

---
+++ LD42/Assets/TimeChangeTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeChangeTrigger : MonoBehaviour
+{
+    public DayTime daytime;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            TimeManager.Instance.SetTime(daytime);
+        }
+    }
+}
