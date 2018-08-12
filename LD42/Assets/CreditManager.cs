8fecef967ae523ba9b3ca703c9bdd8c49914c963
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-12 17:30:11 +0200

    Credits

---
+++ LD42/Assets/CreditManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditManager : SingletonBase<CreditManager> {
+
+    public GameObject Cretit;
+
+    public override void Awake()
+    {
+        ChangeCredit(false);
+    }
+
+    public void ChangeCredit(bool v)
+    {
+        Cretit.SetActive(v);
+    }
+}
--- LD42/Assets/_Scripts/Generic/LoadMainScene.cs
+++ LD42/Assets/_Scripts/Generic/LoadMainScene.cs
@@ -2,15 +2,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadMainScene : MonoBehaviour {
-
+public class LoadMainScene : MonoBehaviour
+{
     public List<string> SceneList;
+    public string TitleScene;
 
     private void Start()
     {
         foreach (var item in SceneList)
         {
-            SceneManager.LoadScene(item,LoadSceneMode.Additive);
-        }       
+            SceneManager.LoadScene(item, LoadSceneMode.Additive);
+        }
+    }
+
+    public void LoadTitle()
+    {
+        SceneManager.LoadScene(TitleScene, LoadSceneMode.Single);
     }
 }
