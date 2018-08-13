4043b19d637fcb165cab944fbd517ffcc29b5e30
Bocsó Gábor <bocsgab@gmail.com>
2018-08-13 22:23:51 +0200

    Safer dialogtexts

--- LD42/Assets/_Scripts/Dialog/DialogText.cs
+++ LD42/Assets/_Scripts/Dialog/DialogText.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 [Serializable]
 public class DialogText
@@ -11,7 +12,22 @@ public class DialogText
 
     public DialogPlacement GetPlacement()
     {
-        return (DialogPlacement)Enum.Parse(typeof(DialogPlacement), Placement);
+        var parsedEnum = DialogPlacement.Top;
+
+        try
+        {
+            var rawEnum = Enum.Parse(typeof(DialogPlacement), Placement, true);
+            if (rawEnum != null)
+            {
+                parsedEnum = (DialogPlacement)rawEnum;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+        }
+
+        return parsedEnum;
     }
 }
 
