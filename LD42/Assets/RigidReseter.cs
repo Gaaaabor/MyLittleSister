ee773279d4c028929960b8e110fa57b699939efc
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-13 20:05:55 +0200

    fixes

---
+++ LD42/Assets/RigidReseter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidReseter : MonoBehaviour
+{
+    public Vector3 _lastPos;
+    public Quaternion _lastRot;
+    
+    private void Awake()
+    {
+        _lastPos = transform.localPosition;
+        _lastRot = transform.localRotation;
+    }
+
+    public void Reset()
+    {
+        transform.localPosition = _lastPos;
+        transform.localRotation = _lastRot;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
