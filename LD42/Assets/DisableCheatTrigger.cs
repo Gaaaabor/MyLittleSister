7206275ebe7bb25a6c854ab9b15910e51b1746de
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-13 18:36:39 +0200

    Cheatmode and  somefix

---
+++ LD42/Assets/DisableCheatTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCheatTrigger : MonoBehaviour
+{
+    public void StartDisableCheat()
+    {
+        DisableCheatManager.Instance.StartDisableCheat();
+    }
+}
--- LD42/Assets/_Scripts/Manager/BuffManager.cs
+++ LD42/Assets/_Scripts/Manager/BuffManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class BuffManager : SingletonBase<BuffManager>
@@ -12,13 +13,25 @@ public class BuffManager : SingletonBase<BuffManager>
     public GameObject CaseSensitivenessBuffUi;
     public Text CaseSensitivenessBuffText;
     public float CaseSensitivenessBuffDuration;
+    private bool _locked;
+
+    internal void LockTime()
+    {
+        _locked = false;
+    }
+
     public bool CaseSensitivenessBuffIsActive;
-    
+
     public override void Awake()
     {
         TimeBuffUi.gameObject.SetActive(false);
         CaseSensitivenessBuffUi.gameObject.SetActive(false);
-        SetTimeScale(1,false);
+        SetTimeScale(1, false);
+    }
+
+    internal void UnlockTime()
+    {
+        _locked = true;
     }
 
     private void Update()
@@ -52,7 +65,7 @@ public class BuffManager : SingletonBase<BuffManager>
 
     private void UpdateTimeBuff()
     {
-        Debug.Log("Update:"+TimeBuffScale);
+        Debug.Log("Update:" + TimeBuffScale);
         if (!TimeBuffIsActive || TimeBuffScale == 1)
         {
             return;
@@ -65,14 +78,15 @@ public class BuffManager : SingletonBase<BuffManager>
         else
         {
             TimeBuffDuration = 0;
-            SetTimeScale(1,false);
+            SetTimeScale(1, false);
         }
 
-        TimeBuffText.text = TimeBuffDuration.ToString("N2");      
+        TimeBuffText.text = TimeBuffDuration.ToString("N2");
     }
 
-    public void SetTimeScale(float scale, bool dontEnable=false)
+    public void SetTimeScale(float scale, bool dontEnable = false)
     {
+        if (_locked) return;
         if (!dontEnable)
         {
             TimeBuffUi.gameObject.SetActive(true);
@@ -89,7 +103,7 @@ public class BuffManager : SingletonBase<BuffManager>
 
         if (TimeBuffScale <= 0)
         {
-            TimeBuffScale = 0;   
+            TimeBuffScale = 0;
         }
 
 
