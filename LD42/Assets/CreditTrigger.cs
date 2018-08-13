b3fcca339d844c27f704ad9efe10098a341a1c6a
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-13 18:21:35 +0200

    Fix

---
+++ LD42/Assets/CreditTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditTrigger : MonoBehaviour {
+    
+	public void ChangeCredit(bool v)
+    {
+        CreditManager.Instance.ChangeCredit(v);
+    }
+}
---
+++ LD42/Assets/DisableCheatManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableCheatManager : SingletonBase<DisableCheatManager>
+{
+    public GameObject Cheat;
+    public void StartDisableCheat()
+    {
+        Cheat.SetActive(true);
+    }
+}
