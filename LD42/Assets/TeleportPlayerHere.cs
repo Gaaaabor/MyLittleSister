8d5b372360491468d503f348dd6f3305bbeccdfa
DESKTOP-V1TD94V\User <attila.kiss@leopoly.com>
2018-08-14 19:42:41 +0200

    Change

---
+++ LD42/Assets/TeleportPlayerHere.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayerHere : MonoBehaviour {
+
+    public void Teleport()
+    {
+        PlayerController.Instance.transform.position = transform.position ;
+        PlayerController.Instance.SetPlayerState(PlayerState.Walking);
+    }
+}
